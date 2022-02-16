3d907aff77b77de17e3a1ae7aed47941af6d50e4
skiado <70379186+skiado@users.noreply.github.com>
2022-02-16 19:33:12 +0200

    Update 03.cpp

--- lectures/01/03.cpp
+++ lectures/01/03.cpp
@@ -1,15 +1,14 @@
-// fahren.cpp
-// demonstrates cin, newline
+// παρουσίαση cin, newline
 #include <iostream>
 using namespace std;
 
 int main(){
     int ftemp;
-    //for temperature in fahrenheit
-    cout << "Enter temperature in fahrenheit: ";
+   
+    cout << "Δώσε τη θερμοκρασία σε βαθμούς fahrenheit: ";
     cin >> ftemp;
     int ctemp = (ftemp-32) * 5 / 9;
-    cout << "Equivalent in Celsius is: " << ctemp << '\n';
+    cout << "Ισοδύναμο σε βαθμούς Celsius: " << ctemp << '\n';
     return 0;
 }
 
