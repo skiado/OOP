0a26253acd9c3d0718b3f6839f1bb17eaa0da6f4
skiado <70379186+skiado@users.noreply.github.com>
2022-02-16 19:31:22 +0200

    Update 02.cpp

--- lectures/01/02.cpp
+++ lectures/01/02.cpp
@@ -1,11 +1,9 @@
-// intvars.cpp
-// demonstrates integer variables
 #include <iostream>
 using namespace std;
 
 int main(){
-    int var1;    //define var1
-    int var2;    //define var2
+    int var1;    //ορισμός var1
+    int var2;    //ορισμός var2
     
     var1 = 20;
     var2 = var1 + 10;
