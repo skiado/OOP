306aaae5b1bd78d41fc6c9eadea92573abf69e9c
skiado <70379186+skiado@users.noreply.github.com>
2022-02-24 10:08:04 +0200

    Add files via upload

---
+++ labs/01/01_04.cpp
@@ -0,0 +1,16 @@
+// C++ 02
+ 
+#include <iostream>
+#include <iomanip>  // for setw
+using namespace std;
+ 
+int main()
+{
+
+	for(int i=1;i<11;i++){
+	    cout << setw(5) << i << setw(6)  << i*i*i<< endl;
+    }
+	return 0;
+}
+
+// g++ -o 01_04 -p 01_04.cpp
